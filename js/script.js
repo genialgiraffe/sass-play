20ba9fec9a404d6c9e112fea392c4742ac0d8865
genialgiraffe <144593571+genialgiraffe@users.noreply.github.com>
2024-02-06 20:11:14 +0000

    Scroll to top when click header

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ function initFilterButton(button, filterValue) {
 
 {/* 
   Sample HTML:
-  <li>
+  <li class="menu-item">
     <h3>Orange juice</h3>
     <p class="price">$1.99</p>
   </li> 
@@ -45,6 +45,7 @@ function buildItemHtml(info) {
     let item = document.createElement("li");
     item.append(name);
     item.append(price);
+    item.classList.add("menu-item")
     return item;
 }
 
@@ -72,6 +73,26 @@ function onClickFilter(button, filterClicked) {
     }
     console.log("New filter selection", CURRENT_FILTER);
     refreshMenuItems();
+    scrollTopMenuItemIntoView();
+}
+
+function scrollTopMenuItemIntoView() {
+    let menuTitle = document.getElementById("menu-title");
+    let menuFilter = document.getElementById("filter");
+    if (menuTitle && menuFilter) {
+        const offset = menuFilter.offsetHeight;
+        const y = menuTitle.getBoundingClientRect().top + window.scrollY - offset;
+        console.log("Scrolling to top of menu", y);
+        window.scrollTo({ top: y, behavior: 'smooth' });
+    } else {
+        console.log("Failed to find elements to scroll", menuTitle, menuFilter)
+    }
+
+    // let items = document.getElementsByClassName("menu-item");
+    // if (items.length > 0) {
+    //     console.log("Scrolling to item", items[0]);
+    //     items[0].scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+    // }
 }
 
 function getAllDrinks() {
